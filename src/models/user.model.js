73f04b1648880ac3a51b5d33fe53c09e3432f96f
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-10 05:56:51 +0500

    func: jwt access an refresh tokens setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@
 
 import mongoose, { Schema } from "mongoose";
 
-import jwt from 'jsonwebtoken'
+import jwt, { sign } from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
 const userSchema = new Schema(
@@ -63,5 +63,31 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+        fullName: this.fullName
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt: sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
