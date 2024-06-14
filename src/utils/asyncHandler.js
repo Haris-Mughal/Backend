ec2cc4f35c220c7749e132bcbb51e5c27293d315
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-14 22:06:57 +0500

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,5 @@
 const asyncHandler = (requestHandler) => {
+
     return async (req, res, next) => {
         await Promise.resolve(requestHandler(req, res, next)).catch((err) => {
             next(err);
